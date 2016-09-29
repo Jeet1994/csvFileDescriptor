4300c3d86c02a0869ba68515d9a09eae3843d0c4
Jeet1994 <pragyaditya.das94@gmail.com>
2016-09-29 17:18:26 +0530

    Fixed the #'export derivatives

--- R/main.R
+++ R/main.R
@@ -3,10 +3,16 @@
 #' This function allows you to do preliminary investigation of the dataset in csv format.
 #' @param Do you love data? Defaults to TRUE
 #' @keywords csv, data, head, tail, summary, dimension, structure, text report, pdf report.
-#' @export
+#' @export DescribeCSV
+#' @export GenPdfReport
+#' @export GenTxtReport
 #' @examples no examples
-#' CSVDescriptor("/path/to/file/file.csv")
+#' DescribeCSV("/path/to/file/file.csv")
+#' GenTxtReport("/path/to/file/file.csv")
+#' GenPdfReport("/path/to/file/Report.txt")
 
+
+#
 #Function for description of the dataset 
 DescribeCSV <- function(.csv) {
   #Taking the csv file.
@@ -41,6 +47,60 @@ DescribeCSV <- function(.csv) {
   
   #Writing the output of the data in the text file.
   
+}
+
+GenTxtReport <- function(.csv)
+{
+  #Command to store the output of the function in a text file 
+  sink('Report.txt')
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  
+  #Taking the csv file.
+  csv <- read.csv(.csv)
+  dimValue <- dim(csv)
+  print("The dimension of the dataset is:")
+  print(dimValue)
+  print("----------------------------------")
+  print("                                  ")
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  #strValue <- str(csv)
+  #Problem with structure printing.
+  #Beautify the output
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  strValue <- utils:::capture.output(str(csv))
+  print("The structure of the dataset is:")
+  print(strValue)
+  print("----------------------------------")
+  print("                                  ")
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  headValue <- head(csv)
+  print("The head of the dataset is:")# or print(headValue) if you prefer
+  print(headValue)
+  print("----------------------------------")
+  print("                                  ")
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  tailValue <- tail(csv)
+  print("The tail of the dataset is:")
+  print(tailValue)
+  print("----------------------------------")
+  print("                                  ")
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  summaryValue <- summary(csv)
+  print("The summary of the dataset is:")
+  print(summaryValue)
+  print("----------------------------------")
+  print("                                  ")
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  description <- return(invisible(list(dimValue, strValue, headValue, tailValue,summaryValue)))
+  sink('Report.txt', append=TRUE)
+}
+
+GenPdfReport<-function(.txt){
+  require(rmarkdown)
+  my_text <- readLines(.txt) 
+  cat(my_text, sep="  \n", file = "Report.Rmd")
+  render("Report.Rmd", pdf_document())
+  file.remove("Report.Rmd") #cleanup
 }
 
 
